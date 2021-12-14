7b571aa4cc1f3468917cdd5affad2625dc551f09
Jake Huggart <jhuggart@shipt.com>
2021-12-14 14:25:09 -0600

    remove loop in proto-gen.sh

--- proto-gen.sh
+++ proto-gen.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-FILES="pb/*.proto"
-
 mkdir -p gen/simple
 
-for file in $FILES; do
-  protoc \
-    -I pb \
-    --go_opt=module=github.com/jhuggart/repro-reflect-failure/gen/simple \
-    --go_out=gen/simple \
-    --go-grpc_opt paths=source_relative \
-    --go-grpc_out=gen/simple \
-    $file
-done
+protoc \
+  -I pb \
+  --go_opt=paths=source_relative \
+  --go_out=gen/simple \
+  --go-grpc_opt=paths=source_relative \
+  --go-grpc_out=gen/simple \
+  pb/*.proto
